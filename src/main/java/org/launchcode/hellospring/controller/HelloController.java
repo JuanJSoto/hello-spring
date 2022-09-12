44b6ae29019b81cebfb5e1943b1fc20ed24565dd
JuanJSoto <JJ3185@hotmail.com>
2022-09-11 22:43:43 -0400

    adding thymeleaf starter code

--- src/main/java/org/launchcode/hellospring/controller/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controller/HelloController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+//@ResponseBody
+//@RequestMapping("hello") //just hello to work its
 public class HelloController {
+    @RequestMapping(value= "hello", method= {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
 
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
     // Handles request at /hello
 //    @GetMapping("hello")
 //    @ResponseBody
@@ -15,40 +25,31 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 //lives at /hello/goodbye
-    @GetMapping("goodbye")
-    //@ResponseBody covered by responseBody on line 7 in the beginning to cover all methods
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
-
-    //handles request of the form /hello?name=LaunchCode
-    //lives at /hello/hello
-   @RequestMapping(method={RequestMethod.GET,RequestMethod.POST},/* value="hello" to reduce redundancy*/)
-    //@ResponseBody
-    public String helloWithQueryParam(@RequestParam String coder) {
-        return "Hello, " + coder + "!!!";
-    }
-
-    //handles request of the form /hello/LaunchCode
-    @GetMapping(/* hello (removed to reduce redundancy*/ "{name}")
-    //@ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "hello, " + name + "!!!!!";
-    }
+//    @GetMapping("goodbye")
+//    //@ResponseBody covered by responseBody on line 7 in the beginning to cover all methods
+//    public String goodbye(){
+//        return "Goodbye, Spring!";
+//    }
+//
+//    //handles request of the form /hello?name=LaunchCode
+//    //lives at /hello/hello
+////s   @RequestMapping(method={RequestMethod.GET,RequestMethod.POST}) /* value="hello" to reduce redundancy*/
+//    //@ResponseBody
+//    public String helloWithQueryParam(@RequestParam String coder) {
+//        return "Hello, " + coder + "!!!";
+//    }
+//
+//    //handles request of the form /hello/LaunchCode
+//    @GetMapping(/* hello (removed to reduce redundancy*/ "{name}")
+//    //@ResponseBody
+//    public String helloWithPathParam(@PathVariable String name){
+//        return "hello, " + name + "!!!!!";
+//    }
 //lives at hello/form
     @GetMapping("form")
-    //@ResponseBody
+    //@ResponseBody templates don't require responseBody
     public String helloForm(){
-        String html =
-            "<html>" +
-                "<body>" +
-                    "<form method='get' action='/hello'>" + //submit a request to /hello was form action
-                    "<input type='text' name='coder'>" +
-                    "<input type='submit' value='Greet me!'" +
-                    "</form>" +
-                    "</body>" +
-                    "</html>";
-        return html;
+        return "form";
     }
 
 //    @GetMapping("hello")
